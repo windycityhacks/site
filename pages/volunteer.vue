0010c517fb48237d11ca5d6f90aba0864a43f304
andrew <me@andreww.co>
2024-05-03 17:08:50 -0500

    volunteer form

---
+++ pages/volunteer.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+await navigateTo(
+    'https://docs.google.com/forms/d/e/1FAIpQLSc6MSulPPQN3eTc0f9ivFZnjd0ZE7jNaFiSUapjjj4TqN_HyA/viewform',
+    {
+        external: true,
+    }
+)
+
+</script>
