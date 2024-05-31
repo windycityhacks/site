2bb2f6318d57dffffce1ac1bb476bb08e9d1042f
andrew <me@andreww.co>
2024-05-30 21:12:45 -0500

    fix mobile

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -6,7 +6,7 @@
                 <div class="w-full mt-6 bg-white p-4 rounded-lg shadow-lg">
                     <h2 class="text-left font-bouquet text-4xl text-primary">☝️ Day 1</h2>
                     <div class="mt-4 md:grid grid-cols-2 3xl:grid-cols-3 gap-4">
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">9:00 AM: Arrival</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Welcome to Windy City Hacks! You'll arrive at 
@@ -17,25 +17,25 @@
                                 >Towne Place West</a>, and sign in with your student ID. We'll be checking at the door.
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">9:30 AM: Opening Ceremony</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 The Windy City Hacks opening ceremony will begin! Tune in to learn about our event prompt, meet our sponsors, and all the workshops we have planned.
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">10:15-10:30 AM: Team Formation</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 The opening ceremony will end, and you'll begin organizing your teams! Make sure to register your team with the Team Registration form, and find a cozy spot to hole up for the few hours.
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">11:00 AM: Hacking Begins</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 You'll start hacking with your teams! Make sure to follow the prompt shared during the opening ceremony, and be ready to work together to build something amazing!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">12:00 PM: Git & Github Workshop</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Learn how to use 
@@ -53,49 +53,49 @@
                                 to collaborate on your projects. You'll also be given access to the <a href="https://education.github.com/pack" class="hover:cursor-pointer hover:underline decoration-wavy decoration-secondary text-primary">Github Student Developer Pack</a>, with a ton of free tools for your project!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">1:00 PM: Snack Time</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Hungry from all that hacking? We'll be offering snacks at one! Grab a bite to eat and stay hydrated while you work on your projects.
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">2:00 PM: ML/AI Workshop</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Ever wanted to learn how to incorporate machine learning and artificial intelligence into your projects?  In this workshop, you'll get a fundamental understanding, learn how they are applied in real-world scenarios, and get hands-on experience with basic ML models.
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">3:30 PM: Personal Website Workshop</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Want to show off your projects to the world? In this workshop, you'll learn how to build a personal website using HTML, CSS, and JavaScript, and host it with Github Pages! You'll also get 🧋 <span class="text-primary">free Boba Tea</span> during dinner (if you complete the workshop)
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">5:30 PM: Dinner</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Time to eat! Take a break from all your hard work, and enjoy some delicious pizza with your team. And, if you completed the personal website workshop, you'll get 🧋 <span class="text-primary">free Boba Tea</span>!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">7:30 PM: Dessert</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 While you're hacking away, you can grab a sweet treat to end your day! 
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">8:00 PM: Game Development Workshop</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Ever wanted to build your own game? In this workshop, you'll learn how to go behinds the scenes in Roblox Studio to build whatever your heart desires, and maybe incorporate it into your project!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">9:30 PM: Typing Contest</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Step away from your project and have some fun! You'll be competing against other attendees to see who can type the fastest in a TypeRacer tournament, and whoever wins will get a prize.
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">10:00 PM: Head Home</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 It's been a long day, you'll want to head home and recharge for Day 2. Or, you can keep hacking away with your friends online. We'll see you tomorrow!
@@ -104,7 +104,7 @@
                     </div>
                     <h2 class="mt-8 text-left font-bouquet text-4xl text-primary">✌️ Day 2</h2>
                     <div class="mt-4 md:grid grid-cols-2 3xl:grid-cols-3 gap-4">
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">8:30 AM: Breakfast</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Welcome back for Day 2. You'll head on back to
@@ -115,25 +115,25 @@
                                 >Towne Place West</a>, and enjoy a bagel breakfast with your team to kick off the final stretch!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">10:00 AM: Project Submission Deadline</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 You'll submit your project to us by 10:00 AM sharp. The organizers and a select group of industry judges will review them all based on a standard rubric. In the mean-time, relax and be proud of yourselves!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">10:00 AM: Project Submission Deadline</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 You'll submit your project to us by 10:00 AM sharp. The organizers and a select group of industry judges will review them all based on a standard rubric. In the mean-time, relax and be proud of yourselves!
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">11:30 AM: Closing Ceremony</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 You'll join us one last time to celebrate the end of Windy City Hacks! We'll be giving out some awesome prizes to the winners (and some superlatives).
                             </p>
                         </div>
-                        <div class="border-2 border-primary/75 p-4 rounded-lg">
+                        <div class="border-2 mt-4 md:mt-0 border-primary/75 p-4 rounded-lg">
                             <h3 class="text-left text-3xl text-primary">12:00 PM: Leave</h3>
                             <p class="text-left text-lg text-black-500 mt-1">
                                 Wow, Windy City Hacks flew by quick! We're so glad you came, and hope you had a great time. See you next year!
