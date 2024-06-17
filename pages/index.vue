75386535492ab600569418c708ae977fcf5b3e0c
andrew <me@andreww.co>
2024-06-17 18:03:42 -0500

    add angaza sponsor & optimize images

--- pages/index.vue
+++ pages/index.vue
@@ -81,6 +81,7 @@
                     <a href="https://desmos.com/" target="_blank"><img class="mx-auto max-h-12 mb-12 lg:mb-0" src="/sponsors/desmos.png" /></a>
                     <a href="https://codecrafters.io/" target="_blank"><img class="mx-auto max-h-16 mt-4" src="/sponsors/codecrafters.png" /></a>
                     <a href="https://hackclub.com/" target="_blank"><img class="mx-auto max-h-24" src="/sponsors/hackclub.png" /></a>
+                    <a href="https://www.angazacenter.org/" target="_blank"><img class="mx-auto max-h-36" src="/sponsors/angaza.png" /></a>
                 </div>
                 <p class="mt-8 pb-4 text-center">This could be <span class="text-secondary">YOU</span>! <a href="mailto:andrew@windycityhacks.com" class="italic hover:underline decoration-wavy decoration-secondary">Contact us</a> to learn more about sponsorships.</p>
             </div>
